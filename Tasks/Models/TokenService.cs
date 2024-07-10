aa04fef66c9b7bf319bf5b046ecf9d07df41e3d9
ytmandani <ytmandani@outlook.com>
2024-07-10 15:20:24 +0300

    - added TokenService - added jwt authentication and swagger configurations to program.cs - added jwt security key to appsettings - added LoginResponse model

---
+++ Tasks/Models/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using BCrypt.Net;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TasksAPI.Models.Responses;
+
+namespace TasksAPI.Models
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly TasksDBContext _dbContext;
+
+        public TokenService(IConfiguration configuration, TasksDBContext tasksDBContext)
+        {
+            _configuration = configuration;
+            _dbContext = tasksDBContext;
+        }
+
+        public (bool IsValid, LoginResponse? Resposne) GenerateToken(string username, string password)
+        {
+            var user = _dbContext.Users.SingleOrDefault(x => x.Username == username);
+            if (user == null)
+            {
+                return (false, null);
+            }
+
+            var validPassword = BCrypt.Net.BCrypt.Verify(password, user.Password);
+            if (!validPassword)
+            {
+                return (false, null);
+            }
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("username", user.Username),
+                new Claim("userId", user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "Yousef Mandani",
+                audience: "Dalal AlKandari",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials);
+
+            var generatedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return (true, new LoginResponse { Token = generatedToken});
+        }
+    }
+}
--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using TasksAPI.Models;
 
 namespace TasksAPI
@@ -14,13 +18,68 @@ namespace TasksAPI
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "TasksAPI",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    { Name = "Yousef Mandani", Email = "contactemailaddress@domain.com", Url = new Uri("https://google.com") }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                            {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
 
             builder.Services.AddDbContext<TasksDBContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
                 options.LogTo(Console.WriteLine, LogLevel.Information);
             });
+
+            builder.Services.AddScoped<TokenService>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       ValidateIssuerSigningKey = true,
+                       ValidIssuer = "Yousef Mandani",
+                       ValidAudience = "Dalal AlKandari",
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+                   };
+               });
 
             var app = builder.Build();
 
@@ -33,8 +92,9 @@ namespace TasksAPI
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
